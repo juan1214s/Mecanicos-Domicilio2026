42325ba4763ab7d1c9f1ab37e26260a3d4141bfb
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2025-11-30 16:43:04 -0500

    renombre un archivo que esta presentanto error en netlify intento 4

--- src/app/pages.tsx
+++ src/app/pages.tsx
@@ -5,7 +5,7 @@ import Reviews from "../components/reviews";
 import Services from "../components/services/services";
 import GoogleMapComponent from "../components/GoogleMapComponent";
 import CarInteractive from "../components/carInteractive";
-import CTASection from "../components/ctaSection.tsx";
+import CTASection from "../components/ctaSection";
 
 export default function Pages(){
     return(
