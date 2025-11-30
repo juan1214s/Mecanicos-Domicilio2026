abf75bade9ece9ae95ce2cf4a5eeb367eea6f9f6
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2025-11-30 16:17:18 -0500

    Fix file extensions and casing for Netlify

--- src/app/pages.tsx
+++ src/app/pages.tsx
@@ -1,11 +1,12 @@
 import AboutUs from "../components/aboutUs";
 import Banner from "../components/banner";
-import CTASection from "../components/ctaSection";
 import FAQ from "../components/faq";
 import Reviews from "../components/reviews";
-import CarInteractive from "../components/carInteractive";
 import Services from "../components/services/services";
 import GoogleMapComponent from "../components/GoogleMapComponent";
+import CTASection from "../components/ctaSection.tsx";
+import CarInteractive from "../components/carInteractive";
+
 
 
 export default function Pages(){
