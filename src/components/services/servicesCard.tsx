0dadd9d03057f882a9819da59de7782e41498a8d
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2025-12-07 19:59:28 -0500

    Cambie el margin de las tarjetas de servicios 2

--- src/components/services/servicesCard.tsx
+++ src/components/services/servicesCard.tsx
@@ -7,20 +7,21 @@ export default function ServicesCard({
   img,
 }: ServicesCardDto) {
   return (
-    <Card className="cardService-custom bg-white rounded-lg shadow-md hover:shadow-xl hover:scale-105 transition-transform duration-300 overflow-hidden max-w-sm h-[460px] mx-auto border-0">
-      <img
-        src={img}
-        alt={title}
-        className="w-full aspect-square object-cover"
-        loading="lazy"
-      />
+<Card className="cardService-custom bg-white rounded-lg shadow-md hover:shadow-xl hover:scale-105 
+transition-transform duration-300 overflow-hidden max-w-sm h-[420px] mx-auto border-0 flex flex-col">
 
-      <div className="p-5 pb-6 space-y-2"> {/* más padding y separación */}
-        <h5 className="text-xl font-bold text-gray-900">{title}</h5>
-        <p className="text-black text-sm leading-relaxed">
-          {description}
-        </p>
-      </div>
-    </Card>
+  <img
+    src={img}
+    alt={title}
+    className="w-full aspect-square object-cover"
+    loading="lazy"
+  />
+
+  <div className="p-5 flex flex-col gap-3 grow">
+    <h5 className="text-xl font-bold text-gray-900">{title}</h5>
+    <p className="text-black text-sm leading-relaxed">{description}</p>
+  </div>
+</Card>
+
   );
 }
